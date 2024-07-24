1d8cf41ecaa0e46c579d0756aba6b78dd7f25bd0
progradan <169004133+progradan@users.noreply.github.com>
2024-07-24 19:36:21 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import programacion.validacionesdevariables as vali
+
+#print(vali.valint(4,(1,7)))
+#print(vali.valList([2], 2, "len"))
+#print(vali.valcomplex(3+4j,5))
+print(vali.valfloat(4.2,[4,10]))
+
+#import programacion.cripto as casa
+#print(casa.user_dec("F¡H¡G¡"))
+
